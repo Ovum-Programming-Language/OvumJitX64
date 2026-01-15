c273ffa3779a254aebd09e9de9f7b641681b99c7
bialger <bigulov.sasha@gmail.com>
2026-01-16 02:19:51 +0300

    fix: fix offset values in the data buffer

--- jit/machine-code-runner/AsmDataBuffer.cpp
+++ jit/machine-code-runner/AsmDataBuffer.cpp
@@ -1,66 +1,68 @@
 #include <jit/machine-code-runner/AsmDataBuffer.hpp>
 
+#include <cstddef>
+
 namespace ovum::vm::jit {
 
 uint64_t AsmDataBuffer::GetOffset(Register reg) {
   switch (reg) {
     case Register::RAX:
-      return 0;
+      return offsetof(AsmDataBuffer, RegisterRAXDataCell);
       break;
     case Register::RCX:
-      return 8;
+      return offsetof(AsmDataBuffer, RegisterRCXDataCell);
       break;
     case Register::RDX:
-      return 16;
+      return offsetof(AsmDataBuffer, RegisterRDXDataCell);
       break;
     case Register::RSI:
-      return 24;
+      return offsetof(AsmDataBuffer, RegisterRSIDataCell);
       break;
     case Register::RDI:
-      return 32;
+      return offsetof(AsmDataBuffer, RegisterRDIDataCell);
       break;
     case Register::R8:
-      return 40;
+      return offsetof(AsmDataBuffer, RegisterR8DataCell);
       break;
     case Register::R9:
-      return 48;
+      return offsetof(AsmDataBuffer, RegisterR9DataCell);
       break;
     case Register::R10:
-      return 56;
+      return offsetof(AsmDataBuffer, RegisterR10DataCell);
       break;
     case Register::R11:
-      return 64;
+      return offsetof(AsmDataBuffer, RegisterR11DataCell);
       break;
     case Register::RSP:
-      return 72;
+      return offsetof(AsmDataBuffer, RegisterRSPDataCell);
       break;
 #ifdef _WIN32
     case Register::XMM0:
-      return 32;
+      return offsetof(AsmDataBuffer, RegisterXMM0DataCell);
       break;
     case Register::XMM1:
-      return 40;
+      return offsetof(AsmDataBuffer, RegisterXMM1DataCell);
       break;
     case Register::XMM2:
-      return 48;
+      return offsetof(AsmDataBuffer, RegisterXMM2DataCell);
       break;
     case Register::XMM3:
-      return 56;
+      return offsetof(AsmDataBuffer, RegisterXMM3DataCell);
       break;
     case Register::XMM4:
-      return 64;
+      return offsetof(AsmDataBuffer, RegisterXMM4DataCell);
       break;
     case Register::XMM5:
-      return 72;
+      return offsetof(AsmDataBuffer, RegisterXMM5DataCell);
       break;
 #endif
     default:
-      return 80;
+      return GetResultOffset();
   }
 }
 
 uint64_t AsmDataBuffer::GetResultOffset() {
-  return 80;
+  return offsetof(AsmDataBuffer, Result);
 }
 
 } // namespace ovum::vm::jit
